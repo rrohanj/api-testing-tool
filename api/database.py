b0174be9a2887ca8fc1512632270228dea351439
Rohan <rohan_joshi@myyahoo.com>
2026-04-18 14:50:10 +0530

    deployment changes

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # For local development, this creates a file named 'sql_app.db'
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
